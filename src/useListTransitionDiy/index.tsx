cd4320f50823646621270089f85554bfe9330f45
longjuan <longjuan.xy@raycloud.com>
2022-04-14 22:01:45 +0800

    20220414： 我还在写。。。

--- src/useListTransitionDiy/index.tsx
+++ src/useListTransitionDiy/index.tsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { setAnimationFrameTimeout, clearAnimationFrameTimeout } from '@/helpers/setAnimationFrameTimeout'
 interface Props<S = any> {
   list: Array<S>,
-  time: number
+  time: number,
+  keyRef: React.MutableRefObject<number>
 }
 
 interface ListItem<S = any> {
+  state: S
   stage: string
-  key: number
-}
-
-interface ItemKey {
   key: number
 }
 
@@ -17,27 +16,60 @@ type RenderCallback = (state: any, stage: string) => React.ReactNode
 
 const useListTransitionDiy = (props: Props) => {
 
-  const { list, time } = props
+  const { list, keyRef } = props
   const [initList, setInitList] = useState<ListItem[]>(list.map(item => item))
   useEffect(() => {
-   
-    // 需要知道更新了什么 删除了什么
-    const addItems: Array<number | undefined> = list.map(item => {
-      const index = initList.findIndex(init => init.key === item.key)
-      if (index === -1)  return item.key
-    })
-    const deleteItems: Array<number | undefined> = initList.map(init => {
-      const index = list.findIndex(item => item.key === init.key)
-      if (index === -1) return init.key
-    })
+    // initList 跟 List比较 state，计算出新增的和需要删除的，每一次操作应该只有新增 or 删除，不会出现两者都存在的情况
+    // 通过新增 or 删除的数据更新 initList
+    // 如果没有新增 or 删除，就要更新上次操作的数据状态
+    // 新增是立刻添加，删除先要改变状态
 
-    if (addItems.length > 0) {}
-    if (deleteItems.length > 0) {}
-  }, [list, time])
+    // 如果两个列表相等，就更新状态为 from的数据 --> enter
+    // 如果不相等，新增--> 加入状态 from， 删除--> 定时移除
+
+    const adds = list.map(item => {
+      if (initList.findIndex(initItem => initItem.state === item) === -1) {
+        return item
+      }
+    }) || []
+
+    const deletes = initList.map(initItem => {
+      if (list.findIndex(item => item === initItem.state) === -1) {
+        return initItem.state
+      }
+    }) || []
+
+    if (adds.length > 0) {
+        adds.forEach(newItem => {
+           keyRef.current++
+          setInitList(preList => [...preList, { key:keyRef.current, stage: 'from', state: newItem }])
+        })
+        // setAnimationFrameTimeout(() => {
+        //   setInitList(preList => {
+        //    return preList.map(item => {
+        //       if (item.stage === 'from') return {...item, stage: 'enter'}
+        //       return item
+        //     })
+        //   })
+        // })
+    } else if (deletes.length > 0) {
+      setInitList(preList => {
+        return preList.map(preItem => {
+          if (deletes.some(deleteItem => deleteItem === preItem.state)) {
+            if (preItem.stage === 'enter')
+              return {...preItem, stage: 'leave'}
+          }
+          return preItem
+        })
+      })
+    }
+
+
+  }, [list, keyRef, initList])
 
   const transition = (renderCallback: RenderCallback) => {
     return initList.map(item => {
-      return renderCallback(item.key, item.stage)
+      return renderCallback(item.state, item.stage)
     })
   }
   return transition
